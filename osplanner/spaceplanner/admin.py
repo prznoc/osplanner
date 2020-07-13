cdcdb4d569946169f9b193a9bbb85ead75a660fe
P1852053 <przemyslaw.nocon@saint-gobain.com>
2020-07-13 14:48:58 +0200

    Changed model structure

--- osplanner/spaceplanner/admin.py
+++ osplanner/spaceplanner/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Workstand, User, Workweek
 
-# Register your models here.
+admin.site.register(Workstand)
+admin.site.register(User)
--- osplanner/spaceplanner/models.py
+++ osplanner/spaceplanner/models.py
@@ -1,39 +1,47 @@
-from django.db import models
+from django.db import models, transaction
 from django.core.validators import MaxValueValidator, MinValueValidator 
 
+from datetime import datetime, timedelta
 
 class Workstand(models.Model):
     ws_id = models.IntegerField(primary_key=True, unique = True)
-
-    def __str__(self):
-        return str(self.ws_id)
-
-
-class Properties(models.Model):
-    workstand = models.OneToOneField(Workstand, on_delete=models.CASCADE, primary_key = True)
     window = models.IntegerField(default = 1, validators=[MaxValueValidator(3), MinValueValidator(1)])
     noise = models.IntegerField(default = 1, validators=[MaxValueValidator(5), MinValueValidator(1)])
     large_screen = models.BooleanField(default = False)
     is_mac = models.BooleanField(default = False)
-    
+
+    def __str__(self):
+        return str(self.ws_id)
+
+    #@transaction.commit_on_success
+    def save(self, *args, **kwargs):
+        today = datetime.today()
+        next_monday = today + timedelta(days=-today.weekday(), weeks=1)
+        while (next_monday.year == today.year):
+            week = Workweek(workstand = self, start_date = next_monday)
+            week.save()
+            next_monday = next_monday + timedelta(days = 7)
+
+        super(Workstand, self).save(*args, **kwargs)
 
 class User(models.Model):
     us_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
+    window = models.IntegerField(default = 1, validators=[MinValueValidator(1), MaxValueValidator(3)])
+    noise = models.IntegerField(default = 1, validators=[MaxValueValidator(1), MinValueValidator(5)])
+    large_screen = models.BooleanField(default = False)
+    is_mac = models.BooleanField(default = False)
 
     def __str__(self):
         return str(self.name)
 
 class Preference(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key = True)
-    window = models.IntegerField(default = 1, validators=[MinValueValidator(1), MaxValueValidator(3)])
-    noise = models.IntegerField(default = 1, validators=[MaxValueValidator(1), MinValueValidator(5)])
-    large_screen = models.BooleanField(default = False)
-    is_mac = models.BooleanField(default = False)
-
+    
 class Workweek(models.Model):
+    week_id = models.AutoField(primary_key=True)
     workstand = models.ForeignKey(Workstand, on_delete=models.CASCADE)
-    start_date = models.DateField(primary_key = True)
+    start_date = models.DateField() #date of week's monday
     monday = models.ForeignKey(User, default= None, blank = True, null = True, on_delete=models.SET_NULL,
     related_name='monday')
     tuesday = models.ForeignKey(User, default= None, blank = True, null = True, on_delete=models.SET_NULL,
@@ -49,3 +57,9 @@ class Workweek(models.Model):
     sunday = models.ForeignKey(User, default= None, blank = True, null = True, on_delete=models.SET_NULL,
     related_name = 'sunday')
 
+'''
+    class Meta:
+        constraints  = [
+            models.UniqueConstraint(fields=['start_date', 'workstand'], name = "workstand schedule")
+        ]
+'''
