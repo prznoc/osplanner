5527d6ca62e4ab64e3021ba353dec0cf4a68a0fe
P1852053 <przemyslaw.nocon@saint-gobain.com>
2020-07-15 11:10:44 +0200

    Returns favourite workspace if available

--- osplanner/spaceplanner/app_logic/assigner.py
+++ osplanner/spaceplanner/app_logic/assigner.py
@@ -3,43 +3,57 @@ from datetime import datetime, timedelta
 from spaceplanner.models import User, Workweek, Preferences
 
 def assign_next_week(user, weekdays):
-    availability = prepare_availability(weekdays)
+    next_monday = datetime.today() + timedelta(days=-datetime.today().weekday(), weeks=1)
+    availability, slots = prepare_availability(weekdays)
+    results = dict.fromkeys(weekdays, None)
     preferences_set = ["is_mac", "window", "noise", "large_screen"]
     preference = Preferences.objects.get(user = user)
-    
     for preference_name in preferences_set:
         if (getattr(preference, preference_name+"_preference") == 3):
-            availability = filter_workspaces(preference_name, preference, availability)
-    '''
-    slots = availability.keys()
-    for slot in slots:
-        if user.favourite_workspace == slot.workstation and check_match_all_days(slot, preferences_set):
-            return slot.workstation
-    '''
+            availability = filter_workspaces(preference_name, preference, availability, slots)
     
-def check_match_all_days(working_weekdays, slot):
+    p = list(availability.values())
+    results = set(p[0])
+    for s in p[1:]:
+        results.intersection_update(s)
+        slot = Workweek.objects.get(workstation = user.favourite_workspace, start_date = next_monday)
+    if slot in results:
+        return slot.workstation
+    
+    for preference_name in preferences_set:
+        if (getattr(preference, preference_name+"_preference") == 2):
+            availability = filter_workspaces(preference_name, preference, availability, slots)
+    
+def check_match_all_days(working_weekdays, availability):
     pass 
 
 def prepare_availability(weekdays):
     next_monday = datetime.today() + timedelta(days=-datetime.today().weekday(), weeks=1)
     slots = Workweek.objects.filter(start_date = next_monday)
     availability = {}
+    free_slots = set()
     for weekday in weekdays:
         free_workstations = []
         for slot in slots:
             if getattr(slot, weekday) is None:
                 free_workstations.append(slot)
+                free_slots.add(slot)
         availability[weekday] = free_workstations
-    return availability
+    return availability, free_slots
     
-def filter_workspaces(preference_name, preference, availability):
-    new_availability = {}
-    slots = availability.keys()
+def filter_workspaces(preference_name, preference, availability, slots):
+    temp_slots = set()
+   
     for slot in slots:
         workstation = slot.workstation
         if (getattr(preference, preference_name) == getattr(workstation, preference_name)):
-            new_availability[slot] = availability[slot]
-    if new_availability: return new_availability
-    else: return availability
+            temp_slots.add(slot)
+    
+    for day in availability.keys():
+        weekday = availability[day]
+        new_weekday = [x for x in weekday if x in temp_slots]
+        if new_weekday:
+            availability[day] = new_weekday
 
-
+    return availability
+    
--- osplanner/spaceplanner/tests/test_database.py
+++ osplanner/spaceplanner/tests/test_database.py
@@ -13,7 +13,7 @@ class AvailabilityTest(TestCase):
 
     def test_get_free_slots_from_empty_schedule(self):
         working_days = ["monday", "wednesday", "saturday"]
-        availability = assigner.prepare_availability(working_days)
+        availability, slots = assigner.prepare_availability(working_days)
         next_monday = datetime.today() + timedelta(days=-datetime.today().weekday(), weeks=1)
         expected_availability = {}
         for workday in working_days:
@@ -21,6 +21,7 @@ class AvailabilityTest(TestCase):
             slot2 = Workweek.objects.get(start_date = next_monday, workstation = Workstation.objects.get(ws_id = 2))
             expected_availability[workday] = [slot1, slot2]
         self.assertEqual(availability, expected_availability)
+        self.assertEqual(slots, set([slot1, slot2]))
 
     
     def test_get_slots_after_edition(self):
@@ -31,12 +32,13 @@ class AvailabilityTest(TestCase):
         user = User.objects.get(name = "Andrzej")
         slot2.wednesday = user
         slot2.save()
-        availability = assigner.prepare_availability(working_days)
+        availability, slots = assigner.prepare_availability(working_days)
         expected_availability = {}
         slot1 = Workweek.objects.get(start_date = next_monday, workstation = Workstation.objects.get(ws_id = 1))
         expected_availability["monday"] = [slot1, slot2]
         expected_availability["wednesday"] = [slot1]
         self.assertEqual(availability, expected_availability)
+        self.assertEqual(slots, set([slot1, slot2]))
 
 
     def test_get_slots_with_one_empty(self):
@@ -48,14 +50,14 @@ class AvailabilityTest(TestCase):
         slot.wednesday = user
         slot.monday = user
         slot.save()
-        availability = assigner.prepare_availability(working_days)
-        print(availability)
+        availability, slots = assigner.prepare_availability(working_days)
         expected_availability = {}
         workstation = Workstation.objects.get(ws_id = 1)
         slot = Workweek.objects.get(start_date = next_monday, workstation = workstation)
         expected_availability["monday"] = [slot]
         expected_availability["wednesday"] = [slot]
         self.assertEqual(availability, expected_availability)
+        self.assertEqual(slots, set([slot]))
 
 
 class SlotFilteringTest(TestCase):
@@ -66,25 +68,36 @@ class SlotFilteringTest(TestCase):
             large_screen = True, is_mac = True)
     
     def test_filter_matching_workspace(self):
+        next_monday = datetime.today() + timedelta(days=-datetime.today().weekday(), weeks=1)
         preference = Preferences.objects.get(user = User.objects.get(name = "Andrzej"))
         workstation1 = Workstation.objects.get(ws_id = 1)
         workstation1.large_screen = True
         workstation1.is_mac = True
         workstation1.save()
         working_days = ["monday", "wednesday", "saturday"]
-        availability = assigner.prepare_availability(working_days)
-        availability = assigner.filter_workspaces("is_mac", preference, availability)
+        availability, slots = assigner.prepare_availability(working_days)
+        availability = assigner.filter_workspaces("is_mac", preference, availability, slots)
+        slot = Workweek.objects.get(start_date = next_monday, workstation = workstation1)
         expected_availability = {}
-        expected_availability[Workweek.objects.get(week_id = 1)] = working_days
+        for day in working_days:
+            expected_availability[day] = [slot]
         self.assertEqual(availability, expected_availability)
-'''
+
     def test_no_matching_workspaces_found(self):
-        preference = Preferences.objects.get(user = User.objects.get(name = "Andrzej"))
-        working_days = ["monday", "wednesday", "saturday"]
-        availability = assigner.prepare_availability(working_days)
+        next_monday = datetime.today() + timedelta(days=-datetime.today().weekday(), weeks=1)
+        user = User.objects.get(name = "Andrzej")
+        preference = Preferences.objects.get(user = user)
+        workstation = Workstation.objects.get(ws_id = 1)
+        slot = Workweek.objects.get(start_date = next_monday, workstation = workstation)
+        slot.wednesday = user
+        slot.monday = user
+        slot.save()
+        working_days = ["monday", "wednesday"]
+        availability, slots = assigner.prepare_availability(working_days)
         expected_availability = availability
-        availability = assigner.filter_workspaces("is_mac", preference, availability)
+        availability = assigner.filter_workspaces("is_mac", preference, availability, slots)
         self.assertEqual(availability, expected_availability)
+
 
 class SelectingWorkspaceTests(TestCase):
 
@@ -109,4 +122,4 @@ class SelectingWorkspaceTests(TestCase):
         user = User.objects.get(name = "Andrzej")
         selected_workspace = assigner.assign_next_week(user, working_days)
         self.assertEqual(selected_workspace, user.favourite_workspace)
-        '''
+        
