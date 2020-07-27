b79f387916dbe644c44a9f6854275228dfe1901c
P1852053 <przemyslaw.nocon@saint-gobain.com>
2020-07-27 08:58:56 +0200

    Highlight today in Workstation schedule view

--- spaceplanner/tables.py
+++ spaceplanner/tables.py
@@ -23,20 +23,20 @@ class WeekdayColumn(tables.Column):
 
 
 class ScheduleTable(tables.Table):
-    data_range = tables.Column(accessor='monday_date', verbose_name='dates', linkify=("schedule_week", (tables.A("pk"), )))
-    #jeśli chcę guzik trzeba manualnie zdefiniować url w szablonie
+    data_range = tables.Column(accessor='monday_date', verbose_name='Dates', linkify=("schedule_week", (tables.A("pk"), )))
     generate_schedule = tables.TemplateColumn(
         template_name="spaceplanner/schedule_button.html", verbose_name="Get schedule", orderable=False)
 
     def render_data_range(self, record):
         return record.monday_date.strftime('%Y/%m/%d') + " - " + (record.monday_date + timedelta(days=6)).strftime('%Y/%m/%d')
 
+    
     def __init__(self, *args, **kwargs):
         for weekday in list(calendar.day_name):
             self.base_columns[weekday] = WeekdayColumn(accessor=weekday, orderable=False, empty_values=[],
                     verbose_name=weekday)
         super().__init__(*args, **kwargs)
-        
+            
     class Meta:
         template_name = "django_tables2/bootstrap.html"
         exclude = ['monday_date', 'id', 'employee']
@@ -55,11 +55,33 @@ class PreferencesTable(tables.Table):
         exclude = ('id', 'employee')
         model = EmployeePreferences
 
+'''
+class WorkstationWeekdayColumn(tables.Column):
+    def render(self, value, record, column):
+        today = datetime.today()
+        today_weekday = list(calendar.day_name)[today.weekday()]
+        today = today.isocalendar()
+        if getattr(record, 'year') == today[0] and \
+            getattr(record, 'week') == today[1] and \
+                self.verbose_name == today_weekday:
+                    self.attrs = {'td': {'bgcolor': 'lightblue'}}
+        else:
+            column.attrs = {'td': {}}
+        if not value:
+            return '---'
+        return value
+'''
 
 class WorkstationsScheduleTable(tables.Table):
 
     workstation = tables.Column(verbose_name="Workstation ID")
-
+    
+    def __init__(self, *args, **kwargs):
+        for weekday in list(calendar.day_name):
+            self.base_columns[weekday] = WeekdayColumn(accessor=weekday, orderable=False, empty_values=[],
+                    verbose_name=weekday)
+        super().__init__(*args, **kwargs)
+    
     class Meta:
         model = Workweek
         fields = ['workstation','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
